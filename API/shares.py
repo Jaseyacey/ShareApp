10bcf3dc8264a26b42f9117c1b59984bec5e8de5
Jason Beedle <jbeedle@gmail.com>
2020-12-24 21:45:41 +0000

    initial commit

---
+++ API/shares.py
@@ -0,0 +1,11 @@
+from yahoofinancials import YahooFinancials
+import yfinance as yf
+import pandas as pd
+
+# tsla_df = yf.download('TSLA',
+#                       start='2019-01-01',
+#                       end='2019-12-31',
+#                       progress=False)
+# tsla_df.head()
+
+# print(tsla_df)
